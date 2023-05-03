24c0c156346ade6d6cf97985803360fcc79a4255
pedrozc90 <pedrozc90@gmail.com>
2023-05-03 13:40:45 -0300

    asserting oUF

--- oUF_RaidDebuffs.lua
+++ oUF_RaidDebuffs.lua
@@ -1,5 +1,66 @@
+--[[
+# Element: Raid Debuffs
+
+Adds support for an element that updates and displays the player's atonement buff with a status bar widget.
+
+## Widgets
+
+-	`RaidDebuffs`: A frame to hold a 'Button' representing debuffs on raid unit.
+
+## Sub-Widgets
+
+-	`icon`	: A `Texture` to represent spell icon.
+-	`cd`	: A `Cooldown` to represent spell duration. 
+-	`time`	: A `FontString` to represent spell duration.
+-	`count`	: A `FontString` to represent spell duration.
+
+## Example implementation
+
+```lua
+-- create raid debuff
+local RaidDebuffs = CreateFrame("Frame", nil, Health)
+RaidDebuffs:SetPoint("CENTER", Health, "CENTER", 0, 0)
+RaidDebuffs:SetHeight(32)
+RaidDebuffs:SetWidth(32)
+RaidDebuffs:SetFrameLevel(Health:GetFrameLevel() + 10)
+
+-- raid debuff icon texture
+RaidDebuffs.icon = RaidDebuffs:CreateTexture(nil, "ARTWORK")
+RaidDebuffs.icon:SetTexCoord(.1, .9, .1, .9)
+RaidDebuffs.icon:SetInside(RaidDebuffs)
+
+-- raid debuff cd
+RaidDebuffs.cd = CreateFrame("Cooldown", nil, RaidDebuffs, "CooldownFrameTemplate")
+RaidDebuffs.cd:SetInside(RaidDebuffs, 1, 0)
+RaidDebuffs.cd:SetReverse(true)
+RaidDebuffs.cd:SetHideCountdownNumbers(true)
+RaidDebuffs.cd:SetAlpha(.7)
+RaidDebuffs.cd.noOCC = true
+RaidDebuffs.cd.noCooldownCount = true
+
+-- raid debuffs options
+RaidDebuffs.onlyMatchSpellID = true
+RaidDebuffs.showDispellableDebuff = true
+--RaidDebuffs.forceShow = true
+
+-- raid debuff timer
+RaidDebuffs.time = RaidDebuffs:CreateFontString(nil, "OVERLAY")
+RaidDebuffs.time:SetFont(C.Medias.Font, 12, "OUTLINE")
+RaidDebuffs.time:SetPoint("CENTER", RaidDebuffs, 1, 0)
+
+-- raid debuff stacks
+RaidDebuffs.count = RaidDebuffs:CreateFontString(nil, "OVERLAY")
+RaidDebuffs.count:SetFont(C.Medias.Font, 12, "OUTLINE")
+RaidDebuffs.count:SetPoint("BOTTOMRIGHT", RaidDebuffs, "BOTTOMRIGHT", 2, 0)
+RaidDebuffs.count:SetTextColor(1, .9, 0)
+
+-- register with oUF
+self.RaidDebuffs = RaidDebuffs
+```
+--]]
 local _, ns = ...
 local oUF = ns.oUF or oUF
+assert(oUF, "oUF RaidDebuffs was unable to locate oUF install.")
 
 local _G = _G
 local addon = {}
